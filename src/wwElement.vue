4c52806c8039842443b47b65d5a5d99bd8069c87
Alexis JC <alexisjc.dev@gmail.com>
2022-05-30 14:04:05 +0200

    feat: calendar base component

---
+++ src/wwElement.vue
@@ -0,0 +1,260 @@
+<template>
+  <vue-cal 
+    :key="content.themeColor + '-' + content.startWeekOnSunday"
+    :disable-views="disabledViews" 
+    :active-view="defaultView" 
+    :hide-view-selector="disabledViews.length >= 4" 
+    :hide-weekends="content.hideWeekends"
+    :events="events"
+    :split-days="calendars"
+    :showAllDayEvents="content.showAllDayEvents"
+    :events-count-on-year-view="content.showCountOnYearView"
+    :locale="content.lang"
+    :class="content.themeColor"
+    :style="{...customColorTheme, ...calendarsStyle, ...categoriesStyle}"
+    :time="!content.enableTimelessMode"
+    :timeStep="content.timestep"
+    :timeFrom="content.timeStart*60"
+    :timeTo="content.timeEnd*60"
+    :startWeekOnSunday="content.startWeekOnSunday"
+    @event-click="handleEventClick"
+  />
+</template>
+
+<script>
+import VueCal from 'vue-cal'
+import 'vue-cal/dist/vuecal.css'
+import 'vue-cal/dist/i18n/fr.js'
+import 'vue-cal/dist/i18n/es.js'
+import 'vue-cal/dist/i18n/de.js'
+
+export default {
+  components: { VueCal },
+  props: {
+    content: { type: Object, required: true },
+    /* wwEditor:start */
+    wwEditorState: { type: Object, required: true },
+    /* wwEditor:end */
+  },
+  computed: {
+    customColorTheme() {
+      return {
+        '--calendar-theme-menu-color': this.content.themeMenuColor,
+        '--calendar-theme-title-bar-color': this.content.themeTitleBarColor,
+        '--calendar-theme-cell-today-color': this.content.themeTodayCellColor,
+        '--calendar-theme-cell-selected-color': this.content.themeSelectedCellColor,
+        '--calendar-default-event-color': this.content.defaultEventColor || 'unset',
+      }
+    },
+    defaultView() {
+      return this.content.defaultView
+    },
+    disabledViews() {
+      return [
+        this.content.enableYearsView ? null : 'years',
+        this.content.enableYearView ? null : 'year',
+        this.content.enableMonthView ? null : 'month',
+        this.content.enableWeekView ? null : 'week',
+        this.content.enableDayView ? null : 'day'
+      ].filter(view => !!view)
+    },
+    calendars() {
+      if(!Array.isArray(this.content.calendars)) return []
+      return this.content.calendars.map((cal, index) => ({
+        id: wwLib.resolveObjectPropertyPath(cal, this.content.calendarIdPath || 'id') || '',
+        label: wwLib.resolveObjectPropertyPath(cal, this.content.calendarLabelPath || 'label') || '',
+        color: wwLib.resolveObjectPropertyPath(cal, this.content.calendarColorPath || 'color') || null,
+        class: 'split-' + index
+      }))
+    },
+    calendarsStyle() {
+      return this.calendars.reduce((acc, next, index) => ({
+        ...acc,
+        ['--calendar-split-' + index + '-bg-color']: next.color
+      }), {})
+    },
+    categories() {
+      if(!Array.isArray(this.content.categories)) return []
+      return this.content.categories.map((cat, index) => ({
+        name: wwLib.resolveObjectPropertyPath(cat, this.content.categoryNamePath || 'name') || '',
+        color: wwLib.resolveObjectPropertyPath(cat, this.content.categoryColorPath || 'color') || null,
+        class: 'cat-' + index
+      }))
+    },
+    categoriesStyle() {
+      return this.categories.reduce((acc, next, index) => ({
+        ...acc,
+        ['--calendar-category-' + index + '-bg-color']: next.color
+      }), {})
+    },
+    events() {
+      if(!Array.isArray(this.content.events)) return []
+      const events = this.content.events.map(event => {
+        const category = this.categories.find(cat => cat.name === wwLib.resolveObjectPropertyPath(event, this.content.eventCategoryPath || 'category'))
+        return {
+          start: wwLib.resolveObjectPropertyPath(event, this.content.eventStartPath || 'start') || new Date(),
+          end: wwLib.resolveObjectPropertyPath(event, this.content.eventEndPath || 'end') || new Date(),
+          title: wwLib.resolveObjectPropertyPath(event, this.content.eventTitlePath || 'title') || '',
+          content: wwLib.resolveObjectPropertyPath(event, this.content.eventContentPath || 'content') || '',
+          allDay: wwLib.resolveObjectPropertyPath(event, this.content.eventAllDayPath || 'allDay') || false,
+          split: wwLib.resolveObjectPropertyPath(event, this.content.eventCalendarPath || 'calendar') || null,
+          class: category ? category.class : 'calendar-default-event-color',
+        }
+      })
+      return events
+    }
+  },
+  watch: {
+    /* wwEditor:start */
+    'wwEditorState.boundProps.events'(isBind) {
+      if (!isBind)
+        this.$emit('update:content:effect', {
+            eventStartPath: null,
+            eventEndPath: null,
+            eventTitlePath: null,
+            eventContentPath: null,
+            eventAllDayPath: null,
+            eventCalendarPath: null,
+            eventCategoryPath: null
+        });
+    },
+    'wwEditorState.boundProps.calendars'(isBind) {
+      if (!isBind)
+        this.$emit('update:content:effect', {
+            calendarIdPath: null,
+            calendarLabelPath: null,
+            calendarColorPath: null
+        });
+    },
+    'wwEditorState.boundProps.categories'(isBind) {
+      if (!isBind)
+        this.$emit('update:content:effect', {
+            categoryNamePath: null,
+            categoryColorPath: null
+        });
+    },
+    /* wwEditor:end */
+  },
+  methods: {
+    handleEventClick(event, domEvent) {
+      this.$emit('trigger-event', { name: 'event:click', event: { event: {
+        start: event.start,
+        end: event.end,
+        title: event.title,
+        content: event.content,
+        calendar: event.split,
+        allDay: event.allDay
+      }, domEvent } });
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+:root {
+  --calendar-theme-menu-color: #dd932c;
+  --calendar-theme-title-bar-color: #fad198;
+  --calendar-theme-cell-today-color: #bfbab3;
+  --calendar-theme-cell-selected-color: #8f8c89;
+  --calendar-default-event-color: rgba(248, 248, 248, 0.8);
+
+  --calendar-split-0-bg-color: unset;
+  --calendar-split-1-bg-color: unset;
+  --calendar-split-2-bg-color: unset;
+  --calendar-split-3-bg-color: unset;
+  --calendar-split-4-bg-color: unset;
+  --calendar-split-5-bg-color: unset;
+  --calendar-split-6-bg-color: unset;
+  --calendar-split-7-bg-color: unset;
+  --calendar-split-8-bg-color: unset;
+  --calendar-split-9-bg-color: unset;
+
+  --calendar-category-0-bg-color: unset;
+  --calendar-category-1-bg-color: unset;
+  --calendar-category-2-bg-color: unset;
+  --calendar-category-3-bg-color: unset;
+  --calendar-category-4-bg-color: unset;
+  --calendar-category-5-bg-color: unset;
+  --calendar-category-6-bg-color: unset;
+  --calendar-category-7-bg-color: unset;
+  --calendar-category-8-bg-color: unset;
+  --calendar-category-9-bg-color: unset;
+}
+
+.vuecal--custom-theme {
+  .vuecal__menu, .vuecal__cell-events-count {background-color: var(--calendar-theme-menu-color)}
+  .vuecal__title-bar {background-color: var(--calendar-theme-title-bar-color)}
+  .vuecal__cell--today, .vuecal__cell--current {background-color: var(--calendar-theme-cell-today-color)}
+  &.vuecal:not(.vuecal--day-view) {
+    .vuecal__cell--selected {background-color: var(--calendar-theme-cell-selected-color)}
+  }
+  .vuecal__cell--selected:before {border-color: var(--calendar-theme-cell-selected-color)}
+}
+
+.calendar-default-event-color {
+  background-color: var(--calendar-default-event-color);
+}
+
+.vuecal__event.cat-0 {
+  background-color: var(--calendar-category-0-bg-color);
+}
+.vuecal__event.cat-1 {
+  background-color: var(--calendar-category-1-bg-color);
+}
+.vuecal__event.cat-2 {
+  background-color: var(--calendar-category-2-bg-color);
+}
+.vuecal__event.cat-3 {
+  background-color: var(--calendar-category-3-bg-color);
+}
+.vuecal__event.cat-4 {
+  background-color: var(--calendar-category-4-bg-color);
+}
+.vuecal__event.cat-5 {
+  background-color: var(--calendar-category-5-bg-color);
+}
+.vuecal__event.cat-6 {
+  background-color: var(--calendar-category-6-bg-color);
+}
+.vuecal__event.cat-7 {
+  background-color: var(--calendar-category-7-bg-color);
+}
+.vuecal__event.cat-8 {
+  background-color: var(--calendar-category-8-bg-color);
+}
+.vuecal__event.cat-9 {
+  background-color: var(--calendar-category-9-bg-color);
+}
+
+.vuecal__cell-split.split-0 {
+  background-color: var(--calendar-split-0-bg-color);
+}
+.vuecal__cell-split.split-1 {
+  background-color: var(--calendar-split-1-bg-color);
+}
+.vuecal__cell-split.split-2 {
+  background-color: var(--calendar-split-2-bg-color);
+}
+.vuecal__cell-split.split-3 {
+  background-color: var(--calendar-split-3-bg-color);
+}
+.vuecal__cell-split.split-4 {
+  background-color: var(--calendar-split-4-bg-color);
+}
+.vuecal__cell-split.split-5 {
+  background-color: var(--calendar-split-5-bg-color);
+}
+.vuecal__cell-split.split-6 {
+  background-color: var(--calendar-split-6-bg-color);
+}
+.vuecal__cell-split.split-7 {
+  background-color: var(--calendar-split-7-bg-color);
+}
+.vuecal__cell-split.split-8 {
+  background-color: var(--calendar-split-8-bg-color);
+}
+.vuecal__cell-split.split-9 {
+  background-color: var(--calendar-split-9-bg-color);
+}
+
+</style>
---
+++ ww-config.js
@@ -0,0 +1,548 @@
+export default {
+  editor: {
+    label: {
+      en: "Calendar",
+      fr: "Calendrier",
+    },
+    customStylePropertiesOrder: [
+      'defaultEventColor', 
+      'themeColor', ['themeMenuColor', 'themeTitleBarColor', 'themeTodayCellColor', 'themeSelectedCellColor'], 
+      'defaultView', ['enableYearsView', 'enableYearView', 'enableMonthView', 'enableWeekView', 'enableDayView'],
+      'enableTimelessMode', ['timestep', 'timeStart', 'timeEnd']
+    ],
+    customSettingsPropertiesOrder: [
+      'lang', 'enableMultiCalendar', 
+      'calendars', ['calendarIdPath', 'calendarLabelPath', 'calendarColorPath'],
+      'categories', ['categoryNamePath', 'categoryColorPath'], 
+      'events', ['eventStartPath', 'eventEndPath', 'eventTitlePath', 'eventContentPath', 'eventAllDayPath', 'eventCalendarPath', 'eventCategoryPath'], 
+    ]
+  },
+  triggerEvents: [
+    { name: 'event:click', label: { en: 'On event click' }, event: 
+      { 
+        event: {
+          start: '',
+          end: '',
+          title: '',
+          content: '',
+          calendar: null,
+          allDay: null
+        }, 
+        domEvent: {} 
+      }
+    },
+  ],
+  properties: {
+    defaultEventColor: {
+      label: {
+        en: "Default event color",
+      },
+      type: 'Color',
+      default: '#F23636'
+    },
+    themeColor: {
+      label: {
+        en: 'Theme color',
+      },
+      type: 'TextSelect',
+      options: {
+        options: [
+          {label: 'Gray', value: 'vuecal--gray-theme'}, 
+          {label: 'Green', value: 'vuecal--green-theme'}, 
+          {label: 'Blue', value: 'vuecal--blue-theme'}, 
+          {label: 'Custom', value: 'vuecal--custom-theme'}
+        ],
+      },
+      defaultValue: 'vuecal--gray-theme'
+    },
+    themeMenuColor: {
+      hidden: content => content.themeColor !== 'vuecal--custom-theme',
+      label: {
+        en: 'Views menu'
+      },
+      type: 'Color',
+      defaultValue: '#F1B7B7'
+    },
+    themeTitleBarColor: {
+      hidden: content => content.themeColor !== 'vuecal--custom-theme',
+      label: {
+        en: 'Title bar'
+      },
+      type: 'Color',
+      defaultValue: '#F6DFDF'
+    },
+    themeTodayCellColor: {
+      hidden: content => content.themeColor !== 'vuecal--custom-theme',
+      label: {
+        en: 'Today cell'
+      },
+      type: 'Color',
+      defaultValue: '#F0EFEF'
+    },
+    themeSelectedCellColor: {
+      hidden: content => content.themeColor !== 'vuecal--custom-theme',
+      label: {
+        en: 'Selected cell'
+      },
+      type: 'Color',
+      defaultValue: '#DED4D4'
+    },
+    defaultView: {
+      label: {
+        en: 'Default view'
+      },
+      type: 'TextSelect',
+      options: {
+        options: [
+          {label: 'Years', value: 'years'}, 
+          {label: 'Year', value: 'year'}, 
+          {label: 'Month', value: 'month'}, 
+          {label: 'Week', value: 'week'},
+          {label: 'Day', value: 'day'}
+        ],
+      },
+      defaultValue: 'week'
+    },
+    enableYearsView: {
+      label: {
+        en: 'Years view'
+      },
+      type: 'OnOff',
+      defaultValue: true
+    },
+    enableYearView: {
+      label: {
+        en: 'Year view'
+      },
+      type: 'OnOff',
+      defaultValue: true
+    },
+    enableMonthView: {
+      label: {
+        en: 'Month view'
+      },
+      type: 'OnOff',
+      defaultValue: true
+    },
+    enableWeekView: {
+      label: {
+        en: 'Week view'
+      },
+      type: 'OnOff',
+      defaultValue: true
+    },
+    enableDayView: {
+      label: {
+        en: 'Day view'
+      },
+      type: 'OnOff',
+      defaultValue: true
+    },
+    hideWeekends: {
+      label: {
+        en: 'Hide weekends'
+      },
+      type: 'OnOff',
+      defaultValue: false
+    },
+    showAllDayEvents: {
+      label: {
+        en: 'Show all day events'
+      },
+      type: 'OnOff',
+      defaultValue: false
+    },
+    showCountOnYearView: {
+      label: {
+        en: 'Show count on year view'
+      },
+      type: 'OnOff',
+      defaultValue: false
+    },
+    enableTimelessMode: {
+      label: {
+        en: 'Timeless mode'
+      },
+      type: 'OnOff',
+      defaultValue: false
+    },
+    timestep: {
+      hidden: content => content.enableTimelessMode,
+      label: {
+        en: 'Timestep (minutes)'
+      },
+      type: 'Number',
+      options: {
+        min: 5,
+        max: 60,
+        step: 5,
+    },
+      defaultValue: 30
+    },
+    timeStart: {
+      hidden: content => content.enableTimelessMode,
+      label: {
+        en: 'Time start (hour)'
+      },
+      type: 'Number',
+      options: {
+        min: 0,
+        max: 23,
+        step: 1,
+    },
+      defaultValue: 8
+    },
+    timeEnd: {
+      hidden: content => content.enableTimelessMode,
+      label: {
+        en: 'Time end (hour)'
+      },
+      type: 'Number',
+      options: {
+        min: 1,
+        max: 24,
+        step: 1,
+      },
+      defaultValue: 20
+    },
+    startWeekOnSunday: {
+      label: {
+        en: 'Start week on Sunday'
+      },
+      type: 'OnOff',
+      defaultValue: false
+    },
+
+    calendars: {
+      section: 'settings',
+      label: {
+        en: 'Calendars',
+      },
+      type: 'Array',
+      bindable: true,
+      options: {
+          item: {
+            type: 'Object',
+            defaultValue: {label: '', color: null, id: ''},
+            options: {
+                item: {
+                    id: {
+                      label: { en: 'Id' },
+                      type: 'Text'
+                    },
+                    label: {
+                        label: { en: 'Label' },
+                        type: 'Text',
+                        options: { placeholder: 'Label' },
+                    },
+                    color: {
+                        label: { en: 'Color' },
+                        type: 'Color',
+                    },
+                },
+            },
+          },
+      },
+      defaultValue: [],
+    },
+    calendarIdPath: {
+      hidden: (content, sidepanelContent, boundProps) =>
+          !boundProps.calendars || !content.calendars || (content.calendars && typeof content.calendars[0] !== 'object'),
+      label: {
+          en: 'Id property',
+      },
+      type: 'ObjectPropertyPath',
+      options: content => {
+          if (!content.calendars.length || typeof content.calendars[0] !== 'object') {
+              return null;
+          }
+
+          return { object: content.calendars[0] };
+      },
+      defaultValue: null,
+      section: 'settings',
+    },
+    calendarLabelPath: {
+      hidden: (content, sidepanelContent, boundProps) =>
+          !boundProps.calendars || !content.calendars || (content.calendars && typeof content.calendars[0] !== 'object'),
+      label: {
+          en: 'Label property',
+      },
+      type: 'ObjectPropertyPath',
+      options: content => {
+          if (!content.calendars.length || typeof content.calendars[0] !== 'object') {
+              return null;
+          }
+
+          return { object: content.calendars[0] };
+      },
+      defaultValue: null,
+      section: 'settings',
+    },
+    calendarColorPath: {
+      hidden: (content, sidepanelContent, boundProps) =>
+          !boundProps.calendars || !content.calendars || (content.calendars && typeof content.calendars[0] !== 'object'),
+      label: {
+          en: 'Color property',
+      },
+      type: 'ObjectPropertyPath',
+      options: content => {
+          if (!content.calendars.length || typeof content.calendars[0] !== 'object') {
+              return null;
+          }
+
+          return { object: content.calendars[0] };
+      },
+      defaultValue: null,
+      section: 'settings',
+    },
+    events: {
+      section: 'settings',
+      label: { en: 'Events' },
+      type: 'Array',
+      bindable: true,
+      options: {
+        item: {
+          type: 'Object',
+          defaultValue: {startDate: null, endDate: null, title: null, content: null, calendar: null},
+          options: {
+              item: {
+                start: {
+                  label: { en: 'Start date' },
+                  placeholder: '2022-01-01 12:00',
+                  type: 'Text'
+                },
+                end: {
+                  label: { en: 'End date' },
+                  placeholder: '2022-01-01 14:00',
+                  type: 'Text'
+                },
+                title: {
+                  label: { en: 'Title' },
+                  type: 'Text'
+                },
+                content: {
+                  label: { en: 'Content' },
+                  type: 'Text'
+                },
+                allDay: {
+                  label: { en: 'All day' },
+                  type: 'OnOff'
+                },
+                calendar: {
+                  label: { en: 'Calendar id' },
+                  type: 'Text'
+                },
+                category: {
+                  label: { en: 'Category name' },
+                  type: 'Text'
+                }
+              }
+          }
+        }
+      },
+      defaultValue: [
+        { start: '2022-01-01 12:00', end: '2022-01-01 14:00', title: 'My first event', content: 'Content of my first event', calendar: null, category: null },
+        { start: '2022-01-02', end: '2022-01-02', title: 'My second event', content: 'Content of my second event', calendar: null, category: null, allDay: true },
+      ],
+    },
+    eventStartPath: {
+      hidden: (content, sidepanelContent, boundProps) =>
+          !boundProps.events || !content.events || (content.events && typeof content.events[0] !== 'object'),
+      label: {
+          en: 'Start date property',
+      },
+      type: 'ObjectPropertyPath',
+      options: content => {
+          if (!content.events.length || typeof content.events[0] !== 'object') {
+              return null;
+          }
+
+          return { object: content.events[0] };
+      },
+      defaultValue: null,
+      section: 'settings',
+    },
+    eventEndPath: {
+      hidden: (content, sidepanelContent, boundProps) =>
+          !boundProps.events || !content.events || (content.events && typeof content.events[0] !== 'object'),
+      label: {
+          en: 'End date property',
+      },
+      type: 'ObjectPropertyPath',
+      options: content => {
+          if (!content.events.length || typeof content.events[0] !== 'object') {
+              return null;
+          }
+
+          return { object: content.events[0] };
+      },
+      defaultValue: null,
+      section: 'settings',
+    },
+    eventTitlePath: {
+      hidden: (content, sidepanelContent, boundProps) =>
+          !boundProps.events || !content.events || (content.events && typeof content.events[0] !== 'object'),
+      label: {
+          en: 'Title property',
+      },
+      type: 'ObjectPropertyPath',
+      options: content => {
+          if (!content.events.length || typeof content.events[0] !== 'object') {
+              return null;
+          }
+
+          return { object: content.events[0] };
+      },
+      defaultValue: null,
+      section: 'settings',
+    },
+    eventContentPath: {
+      hidden: (content, sidepanelContent, boundProps) =>
+          !boundProps.events || !content.events || (content.events && typeof content.events[0] !== 'object'),
+      label: {
+          en: 'Content property',
+      },
+      type: 'ObjectPropertyPath',
+      options: content => {
+          if (!content.events.length || typeof content.events[0] !== 'object') {
+              return null;
+          }
+
+          return { object: content.events[0] };
+      },
+      defaultValue: null,
+      section: 'settings',
+    },
+    eventAllDayPath: {
+      hidden: (content, sidepanelContent, boundProps) =>
+          !boundProps.events || !content.events || (content.events && typeof content.events[0] !== 'object'),
+      label: {
+          en: 'All day property',
+      },
+      type: 'ObjectPropertyPath',
+      options: content => {
+          if (!content.events.length || typeof content.events[0] !== 'object') {
+              return null;
+          }
+
+          return { object: content.events[0] };
+      },
+      defaultValue: null,
+      section: 'settings',
+    },
+    eventCalendarPath: {
+      hidden: (content, sidepanelContent, boundProps) =>
+          !boundProps.events || !content.events || (content.events && typeof content.events[0] !== 'object'),
+      label: {
+          en: 'Calendar ID property',
+      },
+      type: 'ObjectPropertyPath',
+      options: content => {
+          if (!content.events.length || typeof content.events[0] !== 'object') {
+              return null;
+          }
+
+          return { object: content.events[0] };
+      },
+      defaultValue: null,
+      section: 'settings',
+    },
+    eventCategoryPath: {
+      hidden: (content, sidepanelContent, boundProps) =>
+          !boundProps.events || !content.events || (content.events && typeof content.events[0] !== 'object'),
+      label: {
+          en: 'Category name property',
+      },
+      type: 'ObjectPropertyPath',
+      options: content => {
+          if (!content.events.length || typeof content.events[0] !== 'object') {
+              return null;
+          }
+
+          return { object: content.events[0] };
+      },
+      defaultValue: null,
+      section: 'settings',
+    },
+
+    categories: {
+      section: 'settings',
+      label: {
+        en: 'Categories',
+      },
+      type: 'Array',
+      bindable: true,
+      options: {
+          item: {
+            type: 'Object',
+            defaultValue: {name: 'Sport', color: null},
+            options: {
+                item: {
+                    name: {
+                      label: { en: 'Name' },
+                      type: 'Text'
+                    },
+                    color: {
+                        label: { en: 'Color' },
+                        type: 'Color',
+                    },
+                },
+            },
+          },
+      },
+      defaultValue: [],
+    },
+    categoryNamePath: {
+      hidden: (content, sidepanelContent, boundProps) =>
+          !boundProps.categories || !content.categories || (content.categories && typeof content.categories[0] !== 'object'),
+      label: {
+          en: 'Name property',
+      },
+      type: 'ObjectPropertyPath',
+      options: content => {
+          if (!content.categories.length || typeof content.categories[0] !== 'object') {
+              return null;
+          }
+
+          return { object: content.categories[0] };
+      },
+      defaultValue: null,
+      section: 'settings',
+    },
+    categoryColorPath: {
+      hidden: (content, sidepanelContent, boundProps) =>
+          !boundProps.categories || !content.categories || (content.categories && typeof content.categories[0] !== 'object'),
+      label: {
+          en: 'Color property',
+      },
+      type: 'ObjectPropertyPath',
+      options: content => {
+          if (!content.categories.length || typeof content.categories[0] !== 'object') {
+              return null;
+          }
+
+          return { object: content.categories[0] };
+      },
+      defaultValue: null,
+      section: 'settings',
+    },
+
+    lang: {
+      section: 'settings',
+      type: 'TextSelect',
+      label: {
+        en: 'Language'
+      },
+      options: {
+        options: [
+          {label: 'English', value: 'en'}, 
+          {label: 'French', value: 'fr'}, 
+          {label: 'Spanish', value: 'es'},
+          {label: 'German', value: 'de'},
+        ],
+      },
+      defaultValue: 'en'
+    }
+  },
+};
