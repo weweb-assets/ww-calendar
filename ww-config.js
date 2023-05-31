36601db3887abfdb30808b269646ac4e6e9be302
DNA PC <dna.pc.pro@gmail.com>
2023-05-31 17:20:22 +0200

    Misc spaces (ESLint fix)

--- ww-config.js
+++ ww-config.js
@@ -371,13 +371,13 @@ export default {
             //  I'm not sure what's the most efficient way to document this for the end-users
             options: {
                 options: [
-                    {label: 'Monday', value: 1},
-                    {label: 'Tuesday', value: 2},
-                    {label: 'Wednesday', value: 3},
-                    {label: 'Thursday', value: 4},
-                    {label: 'Friday', value: 5},
-                    {label: 'Saturday', value: 6},
-                    {label: 'Sunday', value: 7},
+                    { label: 'Monday', value: 1 },
+                    { label: 'Tuesday', value: 2 },
+                    { label: 'Wednesday', value: 3 },
+                    { label: 'Thursday', value: 4 },
+                    { label: 'Friday', value: 5 },
+                    { label: 'Saturday', value: 6 },
+                    { label: 'Sunday', value: 7 },
                 ],
             },
             responsive: true,
